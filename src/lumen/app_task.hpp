4603a43824d348e6b95c6f6f3ff62d51f66ee8b3
loganfin <finley4.logan@gmail.com>
2024-03-31 21:34:19 -0700

    Add comment

--- src/lumen/app_task.hpp
+++ src/lumen/app_task.hpp
@@ -2,6 +2,12 @@
 
 namespace lumen {
 
+/** The main task that handles the firmware.
+ *
+ * Creates and manages the HTTP server, among other things.
+ *
+ * \param parameters Unused, but required for FreeRTOS tasks.
+ */
 void app_task(void* parameters);
 
 } // namespace lumen
