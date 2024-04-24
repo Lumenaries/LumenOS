827f47f288238b26b95ba567489cbb530568f082
jennaluz <jpura827@gmail.com>
2024-04-24 15:15:50 -0700

    Check length of team name

--- src/lumen/web/handler/football.cpp
+++ src/lumen/web/handler/football.cpp
@@ -82,7 +82,7 @@ esp_err_t football_put(httpd_req_t* request)
         if (request_json["teamOne"].contains("name")) {
             auto name = request_json["teamOne"]["name"];
 
-            if (name.is_string()) {
+            if (name.is_string() && name.size() <= 6) {
                 football->team_one().name().set_value(name);
             }
         }
@@ -99,7 +99,7 @@ esp_err_t football_put(httpd_req_t* request)
         if (request_json["teamTwo"].contains("name")) {
             auto name = request_json["teamTwo"]["name"];
 
-            if (name.is_string()) {
+            if (name.is_string() && name.size() <= 6) {
                 football->team_two().name().set_value(name);
             }
         }
